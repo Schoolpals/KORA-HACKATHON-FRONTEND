cdbe38898d89790686e405fdf1273e706920f283
belbelbelbel <gronaldchia@gmail.com>
2024-08-17 21:52:47 +0100

    finalised the oublic routing folder and the updates on the form in progresss

--- src/Component/Register.tsx
+++ src/Component/Register.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { MdOutlineEdit } from "react-icons/md";
 import { MdErrorOutline } from "react-icons/md";
-
+import { FaRegEdit } from "react-icons/fa";
 export const Register = () => {
     const [currentPage, setCurrentPage] = useState(0);
     const [currentMemberIndex, setCurrentMemberIndex] = useState(0);
@@ -9,12 +8,12 @@ export const Register = () => {
     const [teamName, setTeamName] = useState('');
     const [projectDescription, setProjectDescription] = useState('');
     
-    const [editMode, setEditMode] = useState<any>(null); // Added state for edit mode
+    const [editMode, setEditMode] = useState<any>(null);
     const [editTeamMode, setTeamEditMode] = useState<any>(null); 
     const [editValue, setEditValue] = useState<any>('');
     const [editTeamvalue, setEditTeamvalue] = useState<any>('');
 
-    const handleMemberChange = (field:any, value:any) => {
+    const handleMemberChange = (field:string, value:string) => {
         const newMembers:any = [...members];
         newMembers[currentMemberIndex][field] = value;
         setMembers(newMembers);
@@ -169,33 +168,33 @@ export const Register = () => {
             )}
             {currentPage === 2 && (
                <div className='fixed inset-0 z-50 bg-gray-800 bg-opacity-50 flex items-center justify-center'>
-                 <div className='h sticky xl:w-[60%] w-[90%] md:w-[80%] rounded-[3rem] mx-auto bg-[#F7F8FC] md:rounded-[1.5rem] inter-fonts'>
-                    <div className='xl:h-[34vw]  justify-center gap-[2vw] overflow-y-auto md:h-[36vw] h-[45vw]   w-full bg-[#F7F8FC] shadow-lg pt-[3vw] flex flex-col gap-[3vw] md:rounded-[10px]'>
+                 <div className='  xl:w-[60%] w-[90%] md:w-[80%] rounded-[3rem] mx-auto bg-[#F7F8FC] md:rounded-[1.5rem] inter-fonts'>
+                    <div className='xl:h-[38vw]  justify-center gap-[2vw] overflow-y-auto md:h-[36vw] h-[45vw]   w-full bg-[#F7F8FC] shadow-lg pt-[3vw] flex flex-col gap-[3vw] md:rounded-[10px]'>
             <div className=' text-[#354962] text-[2vw] w-[80%] mx-auto flex  gap-[1vw] justify-center  font-bold'>
                 <div><MdErrorOutline className='text-yellow-800' /></div>
                 <div className='xl:text-[1.3vw] md:text-[1.4vw] text-[1.7vw]  font-normal w-full  mx-auto'>Before completing your registration please take a moment to review the information ensure that all details are complete</div>
             </div>
-            <div className='flex flex-col gap-[2vw] h-full overflow-y-auto justify-center  w-[80%] mx-auto '>
-                <div className='justify-between relative text-[#354962]  text-[1.5vw] md:text-[1.1vw] gap-[3vw] items-center  pb-[1.3vw] flex font-medium'>
+            <div className='flex flex-col gap-[2vw]  justify-center  w-[80%] mx-auto '>
+                <div className='justify-between relative  text-[#354962]  text-[1.5vw] md:text-[1.1vw] gap-[3vw] items-center  pb-[1.3vw] flex font-medium'>
                     <div className='md:text-[1.5vw] xl:text-[1.3vw] text-[2vw]'>Team Name:</div>
                     <div className=' flex items-center gap-4'>
                        <div className='xl:text-[1.2vw] absolute right-20 text-[1.8vw] md:text-[1.4vw] font-normal fonts-text '>{teamName}</div>
                         <div className='absolute right-4' onClick={() => handleTeamEditClick('teamName')}>    
-                            <MdOutlineEdit className='text-[#2961ED]  text-[1.5vw] cursor-pointer'/>
+                            <FaRegEdit className='text-[#2961ED]  text-[1.5vw] cursor-pointer'/>
                         </div>
                     </div>
                 </div>
                 <div className='w-full mx-auto flex flex-col items-start gap-[3vw]'>
                <div className='w-full mx-auto flex flex-col items-start gap-[3vw]'>
-               <div className=' text-[#354962] text-[1.5vw] md:text-[1.2vw] w-full items-center justify-between flex font-medium'>
-                        <div className='text-[#354962] md:text-[1.6vw] xl:text-[1.3vw] text-[2vw] font-medium '>Members Name:</div>
-                        <div className='flex flex-col items-center justify-between md:gap-4'>   {members.map((member, index) => (
+               <div className=' text-[#354962]  text-[1.5vw] md:text-[1.2vw] w-full items-center justify-between flex font-medium'>
+                        <div className='text-[#354962] md:text-[1.6vw] xl:text-[1.3vw]  text-[2vw] font-medium '>Members Name:</div>
+                        <div className='flex flex-col items-center justify-between md:gap-6 gap-6'>   {members.map((member, index) => (
                             <div className='flex items-center  justify-center relative w-full'>
-                                <div key={index} className='text-[#354962] absolute right-20 xl:text-[1.2vw] text-[1.8vw] md:text-[1.4vw] font-medium fonts-text'>
+                                <div key={index} className='text-[#354962] relative right-20 xl:text-[1.2vw] text-[1.8vw] md:text-[1.4vw] font-medium fonts-text'>
                                     {member.name}
                                 </div>
                                 <div className='absolute right-4'>
-                                <MdOutlineEdit
+                                <FaRegEdit
                                             onClick={() => handleEditClick('name', index)}
                                             className='text-[#2961ED] text-[1.5vw] cursor-pointer'
                                         />
@@ -204,15 +203,15 @@ export const Register = () => {
                         ))}</div>
 
                     </div>
-                    <div className=' text-[#354962] text-[1.5vw] md:text-[1.2vw] w-full justify-between flex font-medium font-medium fonts-text'>
+                    <div className=' text-[#354962] text-[1.5vw] md:text-[1.2vw] w-full justify-between items-center flex font-medium font-medium fonts-text'>
                         <div className='text-[#354962] md:text-[1.5vw] xl:text-[1.3vw] text-[2vw] font-medium'>Email Address:</div>
-                        <div className='flex flex-col items-center justify-center md:gap-4'>   {members.map((member, index) => (
+                        <div className='flex flex-col items-center justify-center md:gap-6 gap-6'>   {members.map((member, index) => (
                             <div className='flex items-center relative w-full'>
-                                <div key={index} className='text-[#354962] absolute right-20  xl:text-[1.2vw] text-[1.8vw] md:text-[1.4vw] font-normal'>
+                                <div key={index} className='text-[#354962] relative right-20  xl:text-[1.2vw] text-[1.8vw] md:text-[1.4vw] font-normal'>
                                     {member.email}
                                 </div>
                                 <div className='absolute right-4'>
-                                <MdOutlineEdit
+                                <FaRegEdit
                                             onClick={() => handleEditClick('email', index)}
                                             className='text-[#2961ED] text-[1.5vw] cursor-pointer'
                                         />
@@ -234,7 +233,6 @@ export const Register = () => {
                     { editMode && (
                         <div className=''>
                             <div className='bg-white p-4 rounded shadow-lg'>
-                                <h2 className='text-lg font-bold mb-2'>Edit {editMode.field}</h2>
                                 <input
                                     type='text'
                                     value={editValue}
@@ -263,7 +261,6 @@ export const Register = () => {
                     {editMode ? "" : editTeamMode && (
                         <div className=''>
                             <div className='bg-white p-4 rounded shadow-lg'>
-                                {/* <h2 className='text-lg font-bold mb-2'>Edit {editTeamMode.field}</h2> */}
                                 <input
                                     type='text'
                                     value={editTeamvalue}
@@ -306,11 +303,12 @@ export const Register = () => {
                                 </div>
                                 <div className='w-[85%] mx-auto flex flex-col items-center'>
                                     <button
-                                        type='button'
+                                        type='submit'
                                         className='w-[15vw] md:rounded-[7px] rounded-0 text-white md:text-[1.6vw] text-[1.6vw] xl:text-[1vw] h-[4vw] md:h-[3vw] bg-[#2961ED]'
-                                        onClick={handleDismiss}
+                                        
                                     >
-                                        Dismiss
+                                        <a href="">Dismiss</a>
+                                        
                                     </button>
                                 </div>
                             </div>
